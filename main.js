d9aea438e1e1d77485cbd3aed75b40f41ba972c8
William <wgepeden@gmail.com>
2022-02-18 21:40:45 -0500

    Liker Lab

--- main.js
+++ main.js
@@ -1,11 +1,45 @@
 // Defining text characters for the empty and full hearts for you to use later.
 const EMPTY_HEART = '♡'
 const FULL_HEART = '♥'
-
+const errorModal = document.querySelector ("#modal")
 // Your JavaScript code goes here!
 
+hideError()
+document.addEventListener("DOMContentLoaded", () => {
+  listenForClick ()
+})
 
+function hideError () {
+  errorModal.classList.add("hidden")
+}
 
+function handleError () {
+  errorModal.classList.remove("hidden")
+  setTimeout (() => {
+   hideError()
+  }, 3000)
+}
+
+function listenForClick () {
+  document.addEventListener('click', (event) => {
+   if (event.target.classList.value === 'like-glyph') {
+     handleResponse(event)
+    }
+  })
+}
+
+function handleResponse (event) {
+  if (event.target.textContent === EMPTY_HEART){
+    event.target.classList.add ('activated-heart')
+    event.target.textContent = FULL_HEART
+    mimicServerCall()
+    .catch (error => handleError(error))
+  }
+  else if (event.target.textContent === FULL_HEART){
+    event.target.classList.remove('activated-heart')
+    event.target.textContent = EMPTY_HEART
+  }
+}
 
 //------------------------------------------------------------------------------
 // Don't change the code below: this function mocks the server response
